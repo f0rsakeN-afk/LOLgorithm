47a317c48dc1b929217f3e43029504f58f41cb4a
f0rsakeN-afk <nar3sh0@gmail.com>
2024-11-15 23:48:10 +0545

    rtk setup done

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 const App = () => {
-  return <div>app</div>;
+  const queryClient = new QueryClient();
+  return <>
+    <QueryClientProvider client={queryClient}>
+      app
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+
+  </>
 };
 
 export default App;
